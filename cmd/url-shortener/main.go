f9ec8bf31b4690909f33bb66fe066b1d26deebd2
Maksim Shirokov <m5d3v@mail.ru>
2023-07-19 22:55:53 +0500

    Adds pretty logs for local env

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"url-shortener/internal/config"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
+	"url-shortener/internal/lib/logger/handlers/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
 
@@ -46,12 +47,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(
-				os.Stdout,
-				&slog.HandlerOptions{Level: slog.LevelDebug},
-			),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(
@@ -69,4 +65,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,19 @@ module url-shortener
 go 1.20
 
 require (
+	github.com/fatih/color v1.15.0
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/ilyakaznacheev/cleanenv v1.4.2
+	github.com/mattn/go-sqlite3 v1.14.17
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+)
+
+require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.4.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )

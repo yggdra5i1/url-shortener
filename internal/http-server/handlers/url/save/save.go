4d7af07ee2a0d12b7a5f0746cdd02c8c71a95d1c
Maksim Shirokov <m5d3v@mail.ru>
2023-07-24 23:05:36 +0500

    Adds decoding and validation of request in save package

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,7 +1,14 @@
 package save
 
 import (
+	"net/http"
 	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/exp/slog"
 )
 
 type URLSaver interface {
@@ -16,4 +23,38 @@ type Request struct {
 type Response struct {
 	resp.Response
 	Alias string `json:"alias"`
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error(validateErr.Error()))
+
+			return
+		}
+	}
 }
